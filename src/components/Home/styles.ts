995724fed4ea1039934ce31b23f3b2b3b50f6eaf
Yazalde Filimone Pinto <yazaldefilimon@gmail.com>
2022-04-01 21:09:52 +0200

    style:create frist style of HomeContainer

--- src/components/Home/styles.ts
+++ src/components/Home/styles.ts
@@ -1,6 +1,25 @@
 import styled from "styled-components";
 
-export const Container = styled.article``;
-export const SubContainer = styled.div``;
-export const Column = styled.div``;
-export const Element = styled.div``;
+export const Container = styled.article`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+export const SubContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(7, 250px);
+`;
+export const Column = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const Element = styled.div`
+  background-color: red;
+  width: 235px;
+  height: 350px;
+  border-radius: 10px;
+`;
