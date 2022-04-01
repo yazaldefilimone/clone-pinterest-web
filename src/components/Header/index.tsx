38b8d3433adbc030300121c61be638f354a7653b
Yazalde Filimone Pinto <yazaldefilimon@gmail.com>
2022-04-01 23:45:58 +0200

    feat:create component Header and add styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,35 @@
 import React, { FunctionComponent } from "react";
-import { Container, SubContainer } from "./styles";
+import {
+  Container,
+  SubContainer,
+  Logo,
+  HeaderList,
+  HeaderButton,
+  Nav,
+} from "./styles";
 
 export const Header: FunctionComponent = () => {
   return (
     <Container>
       <SubContainer>
-        <h1>Yazalde Filimone</h1>
+        <Logo>Pinterest</Logo>
+        <Nav>
+          <HeaderList>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Bussens</a>
+            </li>
+            <li>
+              <a href="#">Blog</a>
+            </li>
+          </HeaderList>
+          <HeaderButton>
+            <button>login</button>
+            <button>sinup</button>
+          </HeaderButton>
+        </Nav>
       </SubContainer>
     </Container>
   );
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,5 +1,54 @@
 import styled from "styled-components";
 
-export const Container = styled.header``;
+export const Container = styled.header`
+  width: 100%;
+`;
 
-export const SubContainer = styled.div``;
+export const SubContainer = styled.div`
+  padding: 3rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+export const Logo = styled.a`
+  font-weight: bold;
+  font-size: 2rem;
+  color: var(--primary-color);
+  cursor: pointer;
+  &:hover {
+    border-bottom: 1px solid var(--primary-color);
+  }
+`;
+export const HeaderList = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-weight: bold;
+  gap: 2rem;
+  a {
+    &:hover {
+      color: var(--primary-color);
+      bor
+    }
+  }
+`;
+export const HeaderButton = styled.div`
+  > button {
+    margin-left: 2rem;
+    font-weight: bold;
+    cursor: pointer;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 0.5rem;
+    &:first-child {
+      background: var(--primary-color);
+      color: var(--white-color);
+    }
+  }
+`;
+export const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 5rem;
+`;
