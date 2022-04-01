4f3754f73d2cc646dc30993d87ea7d094e6cb279
Yazalde Filimone Pinto <yazaldefilimon@gmail.com>
2022-04-01 21:47:38 +0200

    fix:add Head Component of homepage

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,6 +1,7 @@
 import React, { Fragment, FunctionComponent } from "react";
 import { Home } from "../../components/Home";
 import { Header } from "../../components/Header";
+import { Head } from "../../components/Head";
 
 // import { Container } from './styles';
 
@@ -8,6 +9,7 @@ export const HomePage: FunctionComponent = () => {
   return (
     <Fragment>
       <Header />
+      <Head />
       <Home />
     </Fragment>
   );
