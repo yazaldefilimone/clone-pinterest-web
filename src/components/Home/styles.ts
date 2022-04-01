758739f079f2bb046a9b8c59775d16ccd3beae69
Yazalde Filimone Pinto <yazaldefilimon@gmail.com>
2022-04-01 21:25:41 +0200

    style:create style of elements

--- src/components/Home/styles.ts
+++ src/components/Home/styles.ts
@@ -15,11 +15,35 @@ export const Column = styled.div`
   justify-content: center;
   align-items: center;
   flex-direction: column;
+  &:nth-child(1) {
+    margin-top: 0px;
+  }
+  &:nth-child(2) {
+    margin-top: 160px;
+  }
+  &:nth-child(3) {
+    margin-top: 320px;
+  }
+  &:nth-child(4) {
+    margin-top: 480px;
+  }
+  &:nth-child(5) {
+    margin-top: 320px;
+  }
+  &:nth-child(6) {
+    margin-top: 160px;
+  }
+  &:nth-child(7) {
+    margin-top: 0px;
+  }
 `;
 
 export const Element = styled.div`
   background-color: red;
-  width: 235px;
+  width: 236px;
   height: 350px;
-  border-radius: 10px;
+  border-radius: 15px;
+  &:not(:first-child) {
+    margin-top: 16px;
+  }
 `;
