77815194040545c20091824d6a498947a7cb414a
Yazalde Filimone Pinto <yazaldefilimon@gmail.com>
2022-04-02 11:09:40 +0200

    fix:move styles

--- src/components/Head/styles.ts
+++ src/components/Head/styles.ts
@@ -1,5 +1,36 @@
 import styled from "styled-components";
 
-export const Container = styled.section``;
-export const SubContainer = styled.div``;
-export const Title = styled.h2``;
+export const Container = styled.section`
+  margin-top: 120px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  > span {
+    font-size: 60px;
+    font-weight: 600;
+  }
+`;
+export const SubContainer = styled.div`
+  position: relative;
+`;
+export const WrapperContent = styled.div`
+  position: absolute;
+  &:not(:first-child) {
+    display: none;
+  }
+  left: 0px;
+  /* right: 0px; */
+  top: 0px;
+  transform: translateX(-50%);
+`;
+export const Title = styled.h2`
+  font-size: 60px;
+  &:not(:first-child) {
+    display: none;
+  }
+  font-weight: 600;
+  color: var(--primary-color);
+  white-space: nowrap;
+  transition-delay: var(--fade-duration);
+`;
--- src/components/Home/styles.ts
+++ src/components/Home/styles.ts
@@ -4,46 +4,9 @@ export const Container = styled.article`
   display: flex;
   flex-direction: column;
   align-items: center;
+  transition-delay: var(--fade-duration);
 `;
 export const SubContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(7, 250px);
-`;
-export const Column = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  &:nth-child(1) {
-    margin-top: 0px;
-  }
-  &:nth-child(2) {
-    margin-top: 160px;
-  }
-  &:nth-child(3) {
-    margin-top: 320px;
-  }
-  &:nth-child(4) {
-    margin-top: 480px;
-  }
-  &:nth-child(5) {
-    margin-top: 320px;
-  }
-  &:nth-child(6) {
-    margin-top: 160px;
-  }
-  &:nth-child(7) {
-    margin-top: 0px;
-  }
-`;
-
-export const Element = styled.div`
-  background-color: red;
-  width: 236px;
-  height: 350px;
-  border-radius: 15px;
-  &:not(:first-child) {
-    margin-top: 16px;
-  }
 `;
